d7ce02ca28d4cc5b269ab11c82e4a6d7927958bd
SeoHyeon <tjgus1668@naver.com>
2023-11-14 14:16:17 +0900

    Update renamer.py
    Squashed commit of the following:
    
    commit 6b5d31bee0d4076f11021c248ac490835d0c3e14
    Merge: bf40b7d5 edad11a0
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 14:15:07 2023 +0900
    
        Merge branch 'test_branch1' of https://github.com/csh1668/RMK into test_branch1
    
    commit bf40b7d5b9464f180d77703e3823a7470bf4e8c3
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 14:15:03 2023 +0900
    
        Update renamer.py
    
    commit bbefa5f44348b37ef72c6188d1f1f3c8bfac2924
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 13:51:48 2023 +0900
    
        Create 99999.xml
    
        Revert "Create 99999.xml"
    
        This reverts commit bb8121082ab39671edc6cdd8dc0c40b56966e68f.
    
    commit edad11a03d704f28dbb2e214eb92ef0229815412
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 13:55:00 2023 +0900
    
        Revert "Create 99999.xml"
    
        This reverts commit bb8121082ab39671edc6cdd8dc0c40b56966e68f.
    
    commit bb8121082ab39671edc6cdd8dc0c40b56966e68f
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 13:51:48 2023 +0900
    
        Create 99999.xml
    
    commit da823fc956118c15e523c066f3de6d579a0988b5
    Merge: 9c3390ae 984d6948
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 13:42:22 2023 +0900
    
        Delete 02280.xml
    
    commit 9c3390aee1eb4035b8939a58906140f66c75ac2c
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 13:42:14 2023 +0900
    
        test
    
    commit 984d6948711c21fcb9fcf0c61416b73e10ea6b03
    Author: github-actions <github-actions@github.com>
    Date:   Tue Nov 14 04:06:00 2023 +0000
    
        Github Actions - Auto Renamer
    
    commit f01908ff190d422d7200f28d2ff7357ecd1534c4
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 13:05:45 2023 +0900
    
        Create Dummy.xml
    
    commit d7beb13604e9944f70673d94ff1a8bda2c552730
    Author: SeoHyeon <tjgus1668@naver.com>
    Date:   Tue Nov 14 12:51:11 2023 +0900
    
        Update renamer.yml
    
    Update renamer.py
    
    Update renamer.yml

--- renamer.py
+++ renamer.py
@@ -1,6 +1,8 @@
 import os
 
 MAX_INDEX = 100001
+LIMIT_FILENAME = 170
+WHITELISTED_PREFIX = ()     # ('ign%', '이파일은수정하지말아줘' )
 RKM_DATA_PATH = os.path.join(os.getcwd(), 'Data')
 
 
@@ -30,19 +32,48 @@ def get_empty_idx() -> int:
     return -1
 
 
-for (root, dirs, files) in os.walk(RKM_DATA_PATH):
-    for file_name in files:
-        if '.xml' in file_name:
-            if file_name[:-4].isdecimal():
-                set(int(file_name[:-4]), True)
+if __name__ == '__main__':
+    # 이미 사용 중인 파일 이름 탐색
+    cnt_distinguish = 0
+    for (root, dirs, files) in os.walk(RKM_DATA_PATH):
+        for file_name in files:
+            if '.xml' in str.lower(file_name) and file_name[:-4].isdecimal():
+                idx = int(file_name[:-4])
+                if get(idx) is True or idx >= MAX_INDEX:    # 파일명 중복 발생 시 or 인덱스를 초과할 경우
+                    print(f'{os.path.join(root, file_name)}의 파일명이 중복되거나 인덱스를 초과하여, 임시로 temporary{cnt_distinguish}.xml로 변경합니다.')
+                    os.rename(os.path.join(root, file_name), os.path.join(root, f'temporary{cnt_distinguish}.xml'))
+                    cnt_distinguish += 1
+                    continue
+                set(idx, True)
 
-for (root, dirs, files) in os.walk(RKM_DATA_PATH):
-    for file_name in files:
-        if '.xml' in file_name and not file_name[:-4].isdecimal():
-            src = os.path.join(root, file_name)
-            dest = os.path.join(root, '{0:05d}.xml'.format(int(get_empty_idx())))
-            print(f'{src} -> {os.path.basename(dest)}')
-            os.rename(src, dest)
-            set(get_empty_idx())
+    # 파일 이름을 바꿔야 하는 .xml 파일 탐색
+    cnt_changed: int = 0
+    for (root, dirs, files) in os.walk(RKM_DATA_PATH):
+        for file_name in files:
+            if '.xml' in str.lower(file_name) and not str.startswith(file_name, WHITELISTED_PREFIX) and not file_name[:-4].isdecimal():
+                src = os.path.join(root, file_name)
+                dest = os.path.join(root, '{0:05d}.xml'.format(int(get_empty_idx())))
+                print(f'{src} -> {os.path.basename(dest)}')
+                os.rename(src, dest)
+                set(get_empty_idx())
+                cnt_changed += 1
 
-print('Done!')
+    # 지나치게 파일 경로가 긴 파일 탐색
+    cnt_long_filename = 0
+    for (root, dirs, files) in os.walk(RKM_DATA_PATH):
+        for file_name in files:
+            if '.xml' in str.lower(file_name):
+                file_path = os.path.join(root, file_name)
+                file_path = file_path[file_path.index('Data'):]
+                if len(file_path) > LIMIT_FILENAME:
+                    cnt_long_filename += 1
+                    print(f'{LIMIT_FILENAME}자를 초과하는 파일 경로: {file_path}')
+
+    if cnt_changed > 0:
+        print(f'{cnt_changed}개의 파일들이 정상적으로 수정되었습니다.')
+    elif cnt_changed == 0:
+        print('수정할 파일이 없습니다.')
+    
+    if cnt_long_filename > 0:
+        print(f'경고: {cnt_long_filename}개의 파일들이 {LIMIT_FILENAME}자를 초과하는 파일 경로를 가집니다.')
+        exit(1)
