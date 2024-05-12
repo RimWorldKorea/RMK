79218ef395df3b0bee5a3e7ff9857afbf29d8088
ㅇㅇ <150018627+RimWorldKorea@users.noreply.github.com>
2024-05-12 19:23:05 +0900

    [Translated Names] Multiplayer, Prepatcher 호환성 (#835)

--- Data/Translated Names - NS4966146/1.5/Source/Properties/AssemblyInfo.cs
+++ Data/Translated Names - NS4966146/1.5/Source/Properties/AssemblyInfo.cs
@@ -1,19 +1,14 @@
-using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
-using System.Security;
-using System.Security.Permissions;
 
-[assembly: AssemblyTitle("Translated names")]
+[assembly: AssemblyTitle("Translated Names")]
 [assembly: AssemblyDescription("RimWorld mod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MaxZiCode, Rimworld Mod Korean")]
-[assembly: AssemblyProduct("Translated names")]
+[assembly: AssemblyProduct("Translated Names")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("857b9c81-9443-449f-a750-ef3f628a1570")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
--- Data/Translated Names - NS4966146/1.5/Source/TranslatedNames/StaticConstructor.cs
+++ Data/Translated Names - NS4966146/1.5/Source/TranslatedNames/StaticConstructor.cs
@@ -12,26 +12,29 @@ namespace TranslatedNames;
 [StaticConstructorOnStartup]
 public static class StaticConstructor
 {
-	[HarmonyPatch(typeof(PawnBioAndNameGenerator), "GiveAppropriateBioAndNameTo", MethodType.Normal)]
+    [HarmonyPatch(typeof(PawnBioAndNameGenerator), "GiveAppropriateBioAndNameTo", MethodType.Normal)]
     private class PatchNameGiver
     {
         public static void Postfix(Pawn pawn)
 		{
-            if (pawn.Name is NameTriple nameTriple)
+			if (!MultiplayerCompat.checkMultifaction(multiplayerActive)) // 하모니 패치가 동작하는 방식을 정확히는 모르는데, 만약 if(){ return; } 처럼 했을 때 해당 메서드에 붙는 다른 하모니 postfix 같은게 실행되지 않을 가능성이 있나?
 			{
-				string translation = TranslationInfo.GetTranslation(nameTriple.First);
-				string translation2 = TranslationInfo.GetTranslation(nameTriple.Nick);
-				string translation3 = TranslationInfo.GetTranslation(nameTriple.Last);
-				pawn.Name = new NameTriple(translation, translation2, translation3);
-			}
-			else if (!(pawn.Name is NameSingle nameSingle))
-			{
-				Log.Warning("Not both");
-			}
-			else
-			{
-				pawn.Name = new NameSingle(TranslationInfo.GetTranslation(nameSingle.Name), nameSingle.Numerical);
-				Log.Warning("Trying to translate not a NameTriple!");
+				if (pawn.Name is NameTriple nameTriple)
+				{
+					string translation = TranslationInfo.GetTranslation(nameTriple.First);
+					string translation2 = TranslationInfo.GetTranslation(nameTriple.Nick);
+					string translation3 = TranslationInfo.GetTranslation(nameTriple.Last);
+					pawn.Name = new NameTriple(translation, translation2, translation3);
+				}
+				else if (!(pawn.Name is NameSingle nameSingle))
+				{
+					Log.Warning("Not both");
+				}
+				else
+				{
+					pawn.Name = new NameSingle(TranslationInfo.GetTranslation(nameSingle.Name), nameSingle.Numerical);
+					Log.Warning("Trying to translate not a NameTriple!");
+				}
 			}
 		}
 	}
@@ -41,7 +44,7 @@ public static class StaticConstructor
 	{
 		private static void Postfix(Pawn __instance)
 		{
-			if (__instance.def.race.intelligence == Intelligence.Humanlike)
+            if (__instance.def.race.intelligence == Intelligence.Humanlike && !MultiplayerCompat.checkMultifaction(multiplayerActive))
 			{
 				PatchNameGiver.Postfix(__instance);
 			}
@@ -64,26 +67,56 @@ public static class StaticConstructor
 
 	public static readonly string translationPath;
 
-	static StaticConstructor()
-	{
-		Assembly thisAssembly = typeof(StaticConstructor).Assembly;
+    public static bool multiplayerActive = false;
+    static StaticConstructor()
+    {
+        Assembly thisAssembly = typeof(StaticConstructor).Assembly; // 원래 rootPath를 판단하기 위해 Assembly.Location을 사용했는데, Prepatcher 모드와 호환되지 않아서 직접적으로 사용 불가.
 
-        string dllPath = Directory.GetParent(thisAssembly.Location).ToString(); // 이 어셈블리가 위치한 폴더 경로입니다.
+		try
+		{
+            ModMetaData rmkModData = ModLister.GetActiveModWithIdentifier("RMK.translation");
 
-		string rootPath = Directory.GetParent(dllPath).ToString(); // 현재 로드된 Translated Names 모듈의 루트 폴더 경로입니다.
+			string currentRimworldVersion = VersionControl.CurrentVersionStringWithoutBuild; // 현재 실행된 림월드 버전
 
-		string translationsPath = Path.Combine(rootPath, "Translations"); // Translations 폴더 경로입니다.
+            List<LoadFolder> listLoadFolders = rmkModData.loadFolders.FoldersForVersion(currentRimworldVersion).Where(t => t.ShouldLoad == true).ToList(); // 현재 실행중인 림월드 버전에 따른 RMK의 LoadFolders 정보를 획득
+
+            string folderNameInRMK = "Translated Names - NS4966146";
+
+            string loadedPartialPath = listLoadFolders.Where(t => t.folderName.Contains(folderNameInRMK)).FirstOrDefault().folderName;
+            // listLoadFolders에서 이미 버전 분기 경로를 LoadFolders의 정보로 판단했기 때문에, loadedPartialPath를 현재 로드된 정확한 폴더라고 간주할 수 있다.
+
+            rootPath = Path.Combine(rmkModData.RootDir.FullName, loadedPartialPath);
+        }
+		catch
+		{
+			// 나중에 여기에 Translated Names 모듈이 RMK에 있지 않은 채 실행된 상태를 가정하고
+			// rootPath를 설정하는 기능을 구현
+			// 아님 아예 프리패처를 뚫고 원래 어셈블리 위치를 불러오는 방법을 알아오든지 (Mono.Cecil 등?)
+		}
+
+		string translationsPath = Path.Combine(rootPath, "Translations");// Translations 폴더 경로입니다.
 
 		string translationLanguage = GetTranslationLanguage(Path.Combine(translationsPath, "Config.xml"));
 
 		if (!string.IsNullOrEmpty(translationLanguage))
 		{
 			translationPath = Path.Combine(translationsPath, translationLanguage); // 설정된 언어의 Translation 폴더 경로입니다.
-			new Harmony("rimworld.rmk.translatednames.mainconstructor").PatchAll(thisAssembly);
+			new Harmony("RMK.TranslatedNames").PatchAll(thisAssembly);
 		}
-	}
 
-	private static void GetFilesWithNames()
+		ModMetaData modMultiplayer = ModLister.GetActiveModWithIdentifier("rwmt.Multiplayer", true); // Multiplayer 모드가 활성화 상태인지 확인하고 기록합니다.
+        if (modMultiplayer != null)
+        {
+            StaticConstructor.multiplayerActive = modMultiplayer.Active; // 이거 앞에 생성자 이름 IDE에서 안 붙여도 된다고 할텐데 붙여야 됨.
+        }
+        
+		if (multiplayerActive)
+		{
+			MultiplayerCompat.loadMultiplayerTypes();
+        }
+    }
+
+    private static void GetFilesWithNames()
 	{
 		string[] array = new string[6] { "First_Male", "First_Female", "Nick_Male", "Nick_Female", "Nick_Unisex", "Last" };
 		string text = Path.Combine(rootPath, "Debug");
