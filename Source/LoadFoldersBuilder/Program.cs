caab38a4f4b071981d3c2feb04e650d7d7a169fc
ㅇㅇ <150018627+RimWorldKorea@users.noreply.github.com>
2026-04-30 20:44:05 +0900

    MigrationHelper 분리

--- Source/LoadFoldersBuilder/Program.cs
+++ Source/LoadFoldersBuilder/Program.cs
@@ -4,6 +4,7 @@ using System.Buffers;
 using System.Diagnostics;
 using System.Xml.Linq;
 using LoadFoldersBuilder;
+using YamlDotNet.Core;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -88,70 +89,17 @@ using YamlDotNet.Serialization.NamingConventions;
     StartMigration: // 구글 시트에서 LFB로 이주용
     
     Console.WriteLine("\nLoadFolders.Build.yaml 파일로 변환할 tsv 형식의 파일 경로를 입력하십시오.");
-    var InputPath = Console.ReadLine();
+    string TSVPath;
     while (true)
     {
-        if (Path.Exists(InputPath)) break;
+        TSVPath = Console.ReadLine() ?? String.Empty;
+        if (Path.Exists(TSVPath)) break;
+        
         Console.WriteLine("\n입력한 파일 경로가 유효하지 않습니다.");
-        if (InputPath is "-cancel") StopProgram();
+        if (TSVPath is "-cancel") StopProgram();
     }
     
-    string TSVFile = File.ReadAllText(InputPath);
-    var TSVLines = TSVFile.Split(Environment.NewLine, StringSplitOptions.None);
-    
-    //var PIDAndLoadPath = new Dictionary<string, string>();
-    var YamlSerializer = new SerializerBuilder().
-        WithNamingConvention(NullNamingConvention.Instance).
-        ConfigureDefaultValuesHandling(DefaultValuesHandling.Preserve).
-        Build();
-    
-    var YamlBoxes = new Dictionary<string, BuildRuleYamlStructure>();
-    foreach (var Line in TSVLines)
-    {
-        // [0] Directory Designator
-        // [1] Mod Name
-        // [2] WorkshopID
-        // [3] Version Designator
-        // [4] PacakgeID
-        string[] Cut = Line.Split('\t', StringSplitOptions.None);
-        string[] Versions = Cut[3].Split(',');
-        
-        // 구글 시트용 버전 지정자 규칙 반영 필요함
-        // 그런데 그게 필요한 양이 그렇게 많지 않고 한 번만 돌리면 되니 수동으로 하자
-
-        string LoadPathBase = Path.Combine("Data", Cut[0], Cut[1] + " - " + Cut[2]);
-        
-        foreach (var Version in Versions)
-        {
-            string FilePath = LoadPathBase;
-            
-            var YamlBox = new BuildRuleYamlStructure();
-            
-            YamlBox.Metadata.WorkshopID = Cut[2];
-            YamlBox.Metadata.ModName = Cut[1];
-            
-            YamlBox.BuildRule.Binding.PackageID = new [] { Cut[4] };
-            YamlBox.BuildRule.Binding.Dependency = RuleDependency.Independent;
-            if (Version != String.Empty)
-            {
-                YamlBox.BuildRule.Version.Default = Version;
-                FilePath = Path.Combine(FilePath, Version);
-            }
-            FilePath = Path.Combine(FilePath, Statics.BuildYamlFileName);
-
-            try
-            {
-                if (Version.AsSpan().ContainsAny("()[]")) throw new Exception();
-                File.WriteAllText(FilePath, YamlSerializer.Serialize(YamlBox));
-            }
-            catch
-            {
-                Console.WriteLine("\n{0} 생성에 실패했습니다.", FilePath);
-            }
-        }
-    }
-    
-    Console.WriteLine("\n초기 데이터 변환이 완료되었습니다.");
+    MigrationHelper.MigrateFromTSV(TSVPath);
     StopProgram();
     
     Console.WriteLine("\n\e[93m이 문구를 보았다면 희망을 버려라.\x1b[0m");
